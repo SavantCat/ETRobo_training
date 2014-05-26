4a4502be9312978c6a2350215b0a79e7a61c65ed
SavantCat <scanf@me.com>
2014-05-27 00:04:03 +0900

    update

---
+++ MY_ET_switch/rxeflash.sh
@@ -0,0 +1,4 @@
+echo Executing NeXTTool to upload my_etrobo_switch.rxe...
+ /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -download=my_etrobo_switch.rxe
+ /cygdrive/C/cygwin/nexttool/NeXTTool.exe /COM=usb -listfiles=my_etrobo_switch.rxe
+echo NeXTTool is terminated.
